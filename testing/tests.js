0a4e29031adafd2e0bd2eb05cea377f834f78a0f
Michael Conrad <michael@threepears.com>
2016-01-25 15:28:40 -0600

    Add tests

--- testing/tests.js
+++ testing/tests.js
@@ -11,4 +11,31 @@ describe('template strings', function () {
     var string = `abc`;
     expect(string).to.be.a('string');
   });
+
+  it('can evaluate variables', function () {
+    var bar = 'baz';
+    var string = `foo${bar}`;
+
+    expect(string).to.equal('foobaz');
+  });
+
+  it('can execute functions', function () {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+
+    expect(string).to.equal('The date is: ' + date);
+  });
+
+  it('can evaluate expressions', function () {
+    var x = `2 + 2 = ${2 + 2}`;
+
+    expect(x).to.equal('2 + 2 = 4');
+  });
+
+  it('can evaluate multi-line strings', function () {
+    var string = `Hello
+       world`;
+
+    expect(string).to.equal('Hello\n       world');
+  });
 });
